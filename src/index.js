ed884cc378940ef05b91a0cd54b77e1bee8916bf
Nataly <NatGord@rambler.ru>
2019-06-14 18:32:06 +0700

    Настройка

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//require ('./fonts/fonts.scss')
+
+require ('./UI_kit/color/UI_color.js')
+require ('./UI_kit/color/UI_color.pug')
+require ('./UI_kit/color/UI_color.scss')
+
+
+//require ('./UI_kit/cards/UI_cards.js')
+//require ('./UI_kit/cards/UI_cards.pug')
+//require ('./UI_kit/cards/UI_cards.scss')
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    
+  },
+
+ output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          { loader: 'file-loader', options: { name: '[name].html' } },
+          'extract-loader',
+          'html-loader',
+          'pug-html-loader'
+        ] },
+      {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: 'src/postcss.config.js' } }
+        }
+       ]
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: 'src/postcss.config.js' } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    },
+    {
+      test: /\.(ttf|eot|woff|svg|woff2)$/,
+      use: [{
+          loader: 'url-loader',
+          options: {
+              publicPath: './',
+              name: '/fonts/[name].[ext]',
+              limit: 1000
+             }
+             }]
+    }
+  ]
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
+}
