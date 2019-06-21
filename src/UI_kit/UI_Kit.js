133a10c80e295cbef30f44897a11f58416b58d21
Nataly <NatGord@rambler.ru>
2019-06-21 23:48:04 +0700

    Создан файл UI_kit.js туда перенесены require для UIkit в index.js оставлен один основной

---
+++ src/UI_kit/UI_Kit.js
@@ -0,0 +1,12 @@
+require ('./color/UI_color.js')
+require ('./color/UI_color.pug')
+require ('./color/UI_color.scss')
+
+
+require ('./cards/UI_cards.js')
+require ('./cards/UI_cards.pug')
+require ('./cards/UI_cards.scss')
+
+require ('./form/UI_form.js')
+require ('./form/UI_form.pug')
+require ('./form/UI_form.scss')
